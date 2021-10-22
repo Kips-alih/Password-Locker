822a67bdca4b2a409e5e8ae43b5b539a4cea8882
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-23 00:41:44 +0300

    Test cases to save multiple credentials and delete credential

--- credential_test.py
+++ credential_test.py
@@ -44,6 +44,31 @@ class TestCredential(unittest.TestCase):
         self.new_credential.save_credential() # saving the new credential
         self.assertEqual(len(Credential.credential_list),1)
 
+    #test case test_save_multiple_credential to test if we can save multiple credentials in our credential list.
+    def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_credential to check if we can save multiple credential
+            objects to our credential_list
+            '''
+            self.new_credential.save_credential()
+            test_credential = Credential("Test","username","0712345678") # new credential
+            test_credential.save_credential()
+            self.assertEqual(len(Credential.credential_list),2)
+
+    #A test case for the feature to delete contacts
+    def test_delete_credential(self):
+            '''
+            test_delete_contact to test if we can remove a credential from our credential list
+            '''
+            self.new_credential.save_credential()
+            test_credential = Credential("Test","username","0712345678") # new credential
+            test_credential.save_credential()
+
+            self.new_credential.delete_credential()# Deleting a credential object
+            self.assertEqual(len(Credential.credential_list),1)
+
+
+
 
 
 
