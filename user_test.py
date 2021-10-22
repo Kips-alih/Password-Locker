41c5cb9d1eacc9f811b94a7e9a434bccac02915d
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-22 23:39:35 +0300

    Defined tearDown method and test case for saving the user

--- user_test.py
+++ user_test.py
@@ -26,6 +26,24 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,"James")
         self.assertEqual(self.new_user.password,"0712345678")
 
+    #tearDown method that does clean up after each test case has run.
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+    #Test case for saving our user
+    def test_save_user(self):
+
+        '''
+        test_save_user test case to test if the contact object is saved into
+        the user list
+        '''
+        self.new_user.save_user() # saving the new contact
+        self.assertEqual(len(User.user_list),1)
+
+
 
 
 if __name__ == '__main__':
